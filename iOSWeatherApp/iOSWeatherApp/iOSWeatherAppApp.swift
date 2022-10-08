60f05ef2ad190b31923352317ca54aada2b61d77
Bohdan Hawrylyshyn <d3cembry@gmail.com>
2022-10-08 19:56:21 +0300

    init new ver

---
+++ iOSWeatherApp/iOSWeatherApp/iOSWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSWeatherAppApp.swift
+//  iOSWeatherApp
+//
+//  Created by Bohdan Hawrylyshyn on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
